761e1050bf7583e7091880f73e966b8453ed5504
berkay <berkaygursoy@gmail.com>
2020-01-18 22:42:51 +0300

    fix graphql extension data field bug

--- src/routers/ApiRouter.ts
+++ src/routers/ApiRouter.ts
@@ -5,10 +5,15 @@ const router = Router();
 import schema from '@src/graphql/schema';
 
 const extensions = ({ result }) => {
-	const key = Object.keys(result.data)[0];
-	const x = result.data[key];
-	result.data = x;
-	return null;
+	try {
+		if (result.data.__schema) return;
+		const key = Object.keys(result.data)[0];
+		const x = result.data[key];
+		result.data = x;
+		return null;
+	} catch {
+		return null;
+	}
 };
 
 router.use(
